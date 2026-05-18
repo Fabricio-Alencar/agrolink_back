80771c5901fee01b4abe1f2c101e0c24ff213a10
Fabricio <10428537@mackenzista.com.br>
2026-05-17 21:08:29 -0300

    Change database URI to PostgreSQL Azure
    Updated database URI to use PostgreSQL on Azure.

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-import os  # biblioteca para lidar com caminhos de arquivos
+import os
 
-# pega o caminho absoluto da pasta do projeto
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-    # chave secreta usada pelo Flask (sessão, segurança, etc.)
     SECRET_KEY = 'sua-chave-secreta-aqui'
 
-    # define o caminho do banco de dados SQLite
-    # cria um arquivo chamado database.db dentro da pasta instance/
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance/database.db')
+    # =========================
+    # POSTGRESQL AZURE
+    # =========================
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://adminagrolink:+Ravy123@agrolink.postgres.database.azure.com:5432/postgres?sslmode=require"
+    )
 
-    # desativa rastreamento de modificações (economiza memória)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
