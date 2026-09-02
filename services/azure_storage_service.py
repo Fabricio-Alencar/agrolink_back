1cb0d83072eb0209164b493292cbf3ec64527469
Fabricio <10428537@mackenzista.com.br>
2026-09-02 19:59:37 -0300

    Excluir foto do container pt.2

--- services/azure_storage_service.py
+++ services/azure_storage_service.py
@@ -73,3 +73,23 @@ def gerar_url_sas(container_nome, nome_blob, expiracao_minutos=60):
     )
 
     return f"{blob_client.url}?{sas_token}"
+
+def deletar_arquivo(nome_blob, container_nome):
+    if not nome_blob:
+        return
+
+    container_client = blob_service_client.get_container_client(
+        container_nome
+    )
+
+    blob_client = container_client.get_blob_client(nome_blob)
+
+    if blob_client.exists():
+        blob_client.delete_blob()
+        print(
+            f"🗑️ Blob removido: {container_nome}/{nome_blob}"
+        )
+    else:
+        print(
+            f"⚠️ Blob não encontrado: {container_nome}/{nome_blob}"
+        )    
