5d4d407606833f3d0ebf88091f3851e0f583ca99
Fabricio <10428537@mackenzista.com.br>
2026-09-02 15:10:28 -0300

    Configure CORS middleware for FastAPI
    Added CORS middleware configuration to allow specific origins.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.auth_routes import router as auth_router
 from routes.perfil_routes import router as perfil_router
@@ -8,10 +9,23 @@ from routes.negociacoes_routes import router as negociacoes_router
 
 app = FastAPI(title="AgroLink API")
 
+# =========================
+# CONFIGURAÇÃO DO CORS
+# =========================
 
-# =========================
-# ROTAS
-# =========================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:8000",
+        "https://front-agrolink-aff0bvbqd2buhfax.eastus-01.azurewebsites.net",
+        "https://back-agrolink-bmbkepbbdkabdhhd.eastus-01.azurewebsites.net",
+        "https://agro-link.azurewebsites.net",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(perfil_router)
